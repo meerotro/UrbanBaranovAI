5a870bd4e994ac51088ab5d168b3bfca711ee58a
meerotro <82140202+meerotro@users.noreply.github.com>
2024-11-30 00:21:20 +0700

    Add files via upload

---
+++ базовые структуры данных/main.py
@@ -0,0 +1,21 @@
+# 1st program
+result = (9 ** 0.5) * 5
+print(result)
+
+# 2nd program
+logic_result = (9.99 > 9.98) and (1000 != 1000.1)
+print(logic_result)
+
+# 3rd program
+expression1 = 2 * 2 + 2
+expression2 = 2 * (2 + 2)
+comparison = expression1 == expression2
+print(expression1, expression2, comparison)
+
+# 4th program
+num_str = '123.456'
+num = float(num_str)
+shifted = num * 10
+shifted_int = int(shifted)
+first_decimal_digit = shifted_int % 10
+print(first_decimal_digit)
---
+++ базовые структуры данных/task3.py
@@ -0,0 +1,9 @@
+# 3rd program
+# Первое выражение: без приоритета
+expression1 = 2 * 2 + 2
+# Второе выражение: с приоритетом сложения
+expression2 = 2 * (2 + 2)
+# Сравнение выражений
+comparison = expression1 == expression2
+
+print(expression1, expression2, comparison)
